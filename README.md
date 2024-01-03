54088b2b5c3d809c8e4fd2e67aff905985c7d0ad
Gwenn RT <gwenncreative@gwenns-MBP.attlocal.net>
2024-01-02 21:46:13 -0600

    changes made to html, readme, and js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Work Day Scheduler</h1>
 <p>This is a work day scheduler. This scheduler allows a user to save events for each hour of a typical work day (9am to 5pm).</p>
 
-<p>This repository contains starter code from crispy-octo-meme and includes HTML, CSS, JavaScript and JQuery.</p>
+<p>This repository contains starter code from crispy-octo-meme and includes HTML, Bootstrap, CSS, JavaScript and JQuery.</p>
 
 <h1>URL</h1>
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // display the current date and time
 var currentDay = $('#currentDay');
 
@@ -29,29 +25,9 @@ function addClass() {
 
 addClass();
 
-// save content to local storage when clicking save and display
-$('.btn').on('click', function() {
-    localStorage.setItem(".description");
-});
-
-// $(function(){
-
-// });
-
-$(function () {
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-  }); 
- 
- 
+// event listener for click events on the save button and save content to local storage
+// $('.btn').on('click',function(){
+//     var keyName = $this.
+//     var keyValue = $this.
+//     localStorage.setItem(keyName, keyValue);
+// })
